43680a384ff8ca48235bd682da71077686547e25
keyhan-va <keyhan.va@gamil.com>
2019-07-31 14:48:53 +0430

    add cart

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-
 var app = new Vue({
-  el: '#app',
-  data: {
-    maximum: 99,
-    products: null
-  },
-  mounted: function(){
-    fetch('https://hplussport.com/api/products/order/price')
-    .then(response => response.json())
-    .then(data => {
-      this.products = data;
-    })
-  }
+ el: '#app',
+ data: {
+  maximum: 99,
+  products: null,
+  cart: []
+ },
+ methods: function(products){
+   this.cart.push(products);
+ },
+ mounted: function() {
+  fetch('https://hplussport.com/api/products/order/price')
+  .then(response => response.json())
+  .then(data => {
+    this.products = data;
+  })
+ }
 });
